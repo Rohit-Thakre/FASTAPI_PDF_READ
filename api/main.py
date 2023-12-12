85b5c2d53b2109ab1a6d9faedf60fbf177a706da
Rohit Thakre <thake785@gmail.com>
2023-12-12 17:07:33 +0530

    code seprated in functions.

--- api/main.py
+++ api/main.py
@@ -1,88 +1,15 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
 
-# django ----
-from PyPDF2 import PdfReader
 from io import BytesIO
-import re
-# -----------
+from PyPDF2 import PdfReader
 
+
+
+import sbi
+import nbo
+
+from fastapi import FastAPI, UploadFile, File, HTTPException
 app = FastAPI()
 
-def cal(pdf_data: bytes) -> dict:
-    pdf_stream = BytesIO(pdf_data)
-
-    # Use PdfReader to read the PDF data from the BytesIO object
-    pdf = PdfReader(pdf_stream)
-    page_size = len(pdf.pages)
-
-    rows = []
-    months = list()
-    
-    for no in range(page_size): 
-    # for no in range(1): 
-        page_obj = pdf.pages[no]    
-        
-        # start_from 120
-        page_text = page_obj.extract_text()[120:]
-
-        page_list = re.split(r'OMR\n', page_text)
-        # last me ORM add karn hai, since ORM\n se splite kr rahe hai to vo remove kiya hai 
-
-        for line in page_list:
-            try : 
-                line = line+'OMR'
-
-                amt = re.findall(r'\-*\d{1,3}\.\d{1,}\sOMR', line)
-                amount = amt[0]
-
-                float_amount = float(amount.split(' ')[0])
-
-                ac_bal = float(amt[1].split(' ')[0])   
-
-                credite = 0
-                debit = 0
-                if amount[0] =="-":
-                    # debited_sum += float_amount
-                    debit += float_amount
-                else: 
-                    # credited_sum += float_amount
-                    credite += float_amount
-
-                posting_date = re.findall(r'\d{2}\s\w{3}\s\d{4}', line)[0]
-                month = posting_date.split(' ')[1]
-                
-                if month not in months: 
-                    months.append(month)
-
-                rows.append({'month': month, 'credit': credite, 'debit': -debit, "ac_bal" : ac_bal})
-
-
-            except:
-                # print("error occured")
-                pass
-        
-    credit_sum = {}
-    debit_sum = {}
-    saving = {}
-
-    for row in rows:
-        month = row['month']
-        credit = row['credit']
-        debit = row['debit']
-        
-        credit_sum[month] = credit_sum.get(month, 0) + credit
-        debit_sum[month] = debit_sum.get(month, 0) + debit
-
-    for x in credit_sum: 
-        # print(credit_sum[x]-debit_sum[x])
-        # calculating saving  credit - debit (for each month)
-        saving[x] = credit_sum[x] - debit_sum[x]
-
-
-
-    return {"credit_sum": credit_sum, "debit_sum":debit_sum, "saving": saving}
-  
-  
 
 @app.get("/")
 async def read_root():
@@ -90,13 +17,34 @@ async def read_root():
 
 
 @app.post("/upload")
-async def read(file: UploadFile = File(...)):
+async def read(name:str, bank: str, file: UploadFile = File(...) ):
     if file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="Please upload a PDF file")
 
     pdf_data = await file.read()
-    # total_dct = cal(pdf_data)
-    
-    return cal(pdf_data)
 
+
+    # for checking pdf belongs to user or not 
+    pdf_stream = BytesIO(pdf_data)
+    pdf = PdfReader(pdf_stream)
+    page1 = pdf.pages[0]
+    text = page1.extract_text()[:200].lower()
+
+    if name.lower() not in text: 
+        raise HTTPException(status_code=400, detail=f"Pdf file does not belongs to {name}.")
+
+
+    try:
+        if bank.lower() == 'nbo' or bank.lower() == 'national bank of oman': 
+            return nbo.nbo_cal(pdf_data)
+
+        elif bank.lower() == 'sbi' or bank.lower() == 'state bank of india':
+            return sbi.sbi_cal(pdf_data)
+        
+        else:
+            return {'msg': f'can\'t handle {bank}\'s statement at the moment.'}
+    except:
+        return {'error msg': "Error Occured while proccesing pdf file"}
+
+# ------------------------------------------------------
 
---
+++ api/sbi.py
@@ -0,0 +1,53 @@
+# !pip install tabula-py
+import pandas as pd
+import numpy as np
+import re
+import tabula
+
+from io import BytesIO
+
+def sbi_cal(pdf):
+    # dfs = tabula.read_pdf(pdf, pages='all')
+    try:
+        pdf_stream = BytesIO(pdf)
+        
+        dfs = tabula.read_pdf(pdf_stream)
+
+        df = pd.concat([ dfs[page] for page in range(len(dfs))], axis=0)
+        df = df.dropna(subset=['Txn Date', 'Balance'])
+
+        s = pd.Series(list(range(df.shape[0])))
+        # print(s)
+        df = df.set_index([s])
+
+        for row_no in df.index:
+            df["Txn Date"][row_no] = re.findall(r'\b([A-Za-z]+)', df['Txn Date'][row_no])[0]
+            df['Debit'][row_no] = float(str(df["Debit"][row_no]).replace(',', ''))
+            df['Credit'][row_no] = float(str(df["Credit"][row_no]).replace(',', ''))
+            df['Balance'][row_no] = float(str(df["Balance"][row_no]).replace(',', ''))
+
+        total = {}
+
+        for month in list(set(df['Txn Date'])):
+            # print(month)
+            debit_sum = df[df['Txn Date'] == month]['Debit'].sum()
+            credit_sum = df[df['Txn Date'] == month]['Credit'].sum()
+            saving = credit_sum - debit_sum
+            balance_sum = df[df['Txn Date'] == month]['Balance'].sum()
+
+            # print("Debite sum",df[df['Txn Date'] == month]['Debit'].sum())
+            # print("Credit sum", df[df['Txn Date'] == month]['Credit'].sum())
+            # print("Balance sum",df[df['Txn Date'] == month]['Balance'].sum())
+
+            total[month] = {'Debite sum': debit_sum,
+                            'Credite sum' : credit_sum,
+                            'Saving' : saving,
+                            'balance' : balance_sum
+                            }
+
+        return total 
+    
+    except:
+        return {"msg" : "Error Occured while proccesing file"}
+
+        
